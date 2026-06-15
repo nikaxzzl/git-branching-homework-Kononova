99a2ecbadc0e35def2e0c8be728504b0fa553ac2
nikaxzzl <knika150409@yandex.ru>
2026-06-15 23:27:13 +0300

    feat: добавила комментарий  #main.py после подключения subjects.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+# main.py после подключения subjects.py
 from profile import print_profile
 from subjects import print_subjects
-from report import print_report
 
 print("Student Branching App")
 print_profile()
 print_subjects()
-print_report()
